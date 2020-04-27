aaf82b6d609b0937e2f8c288078d0e9d910caf62
Lisa Miller <lisayevonne@outlook.com>
2020-04-27 16:22:44 -0400

    deploy

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Game from "./components/game.js";
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <div className="gameBox">
+          <Game />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
