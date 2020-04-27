259a513543336f344cb8c1e011425931d002e109
Lisa Miller <lisayevonne@outlook.com>
2020-04-27 16:24:07 -0400

    Updates

---
+++ precache-manifest.4f9865ece9559d511d57437422472d2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fec5ca6c67521f39a7c8bbf1d0e38f28",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "bfe7964e8483c3f0272c",
+    "url": "/tictactoe/static/css/main.2cff609a.chunk.css"
+  },
+  {
+    "revision": "0753ffe3c2caaa850505",
+    "url": "/tictactoe/static/js/2.a1d099ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.a1d099ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfe7964e8483c3f0272c",
+    "url": "/tictactoe/static/js/main.3822c756.chunk.js"
+  },
+  {
+    "revision": "8f266c157998e9d5700e",
+    "url": "/tictactoe/static/js/runtime-main.2f446db0.js"
+  },
+  {
+    "revision": "5c285f3dbfd0c0d5d53ffab5e0542bb2",
+    "url": "/tictactoe/static/media/woodbck.5c285f3d.jpg"
+  }
+]);
